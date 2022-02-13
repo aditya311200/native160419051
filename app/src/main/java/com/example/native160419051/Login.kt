4af7055306115da95369883143fdf19ce2e3eed7
nattfeb <s160419080@student.ubaya.ac.id>
2022-02-13 18:22:27 +0700

    define username and password

--- app/src/main/java/com/example/native160419051/Login.kt
+++ app/src/main/java/com/example/native160419051/Login.kt
@@ -2,6 +2,8 @@ package com.example.native160419051
 
 class Login (var username: String, var password: String) {
     override fun toString(): String {
+        username = "ubaya2022"
+        password = "ujicobagit"
         return "$username $password"
     }
 }
